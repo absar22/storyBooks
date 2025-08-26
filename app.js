3645452895142a5fafacaf9dc3f63b51b2a6bd32
Absar Ahmad <absarahmad137@gmail.com>
2025-08-26 15:04:47 +0530

    Setup Google OAuth with Passport, add User model, auth routes, and dynamic Handlebars header

--- app.js
+++ app.js
@@ -4,10 +4,16 @@ const dotenv = require('dotenv')
 const morgan = require('morgan')
 // const exphbs = require('express-handlebars')
 const { engine } = require('express-handlebars') 
+const passport = require('passport')
+const session = require('express-session')
 const connectDB = require('./config/db')
 
 // Load config
 dotenv.config({path: './config/config.env'})
+
+// Passport config
+require('./config/passport')(passport)
+
 
 connectDB()
 
@@ -23,11 +29,25 @@ if(process.env.NODE_ENV === 'developement'){
 app.engine('.hbs', engine({ defaultLayout: 'main', extname: '.hbs' }))
 app.set('view engine', '.hbs')
 
+// Sessions
+app.use(
+  session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: false
+ 
+}))
+
+// Passport Middleware
+app.use(passport.initialize())
+app.use(passport.session())
+
 // Static folder
 app.use(express.static(path.join(__dirname, 'public')))
 
 // Routes
 app.use('/',require('./routes/index'))
+app.use('/auth',require('./routes/auth'))
 
 const PORT  = process.env.PORT || 3000
 
